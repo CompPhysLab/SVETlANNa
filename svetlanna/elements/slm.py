8fe9bfffec5c98e3793b47d66a6a2410cefd1052
djiboshin <64642744+djiboshin@users.noreply.github.com>
2026-02-04 00:20:24 +0300

    Update slm.py

--- svetlanna/elements/slm.py
+++ svetlanna/elements/slm.py
@@ -2,31 +2,53 @@ import torch
 import warnings
 from .element import Element
 from ..simulation_parameters import SimulationParameters
-from ..parameters import OptimizableFloat
+from ..parameters import OptimizableTensor
 from ..wavefront import Wavefront, mul
 from typing import Callable, Tuple, Literal
-from torch.nn.functional import interpolate, relu
+from torch.nn.functional import interpolate
+
+
+def mesh_intersection_indices(
+    x1_left: float,
+    x1_right: float,
+    x2_left: float,
+    x2_right: float,
+    dx: float,
+) -> tuple[slice, slice]:
+    mesh1_left_idx = 0
+    mesh1_right_idx = round((x1_right - x1_left) / dx)
+
+    mesh2_left_idx = round((x2_left - x1_left) / dx)
+    mesh2_right_idx = round((x2_right - x1_left) / dx)
+
+    intersection_start_idx = max(mesh1_left_idx, mesh2_left_idx)
+    intersection_end_idx = min(mesh1_right_idx, mesh2_right_idx)
+
+    if intersection_start_idx > intersection_end_idx:
+        return slice(0), slice(0)
+
+    mesh1_slice = slice(intersection_start_idx, intersection_end_idx + 1)
+    mesh2_slice = slice(
+        intersection_start_idx - mesh2_left_idx,
+        intersection_end_idx - mesh2_left_idx + 1,
+    )
+    return mesh1_slice, mesh2_slice
 
 
 class SpatialLightModulator(Element):
     """A class that described the field after propagating through the
     Spatial Light Modulator with a given phase mask
-
-    Parameters
-    ----------
-    Element : _type_
-        _description_
     """
 
     def __init__(
         self,
         simulation_parameters: SimulationParameters,
-        mask: torch.Tensor,
-        height: OptimizableFloat,
-        width: OptimizableFloat,
-        location: Tuple = (0.0, 0.0),
-        number_of_levels: int = 256,
-        step_function: Callable[[torch.Tensor], torch.Tensor] = relu,
+        mask: OptimizableTensor,
+        height: float,
+        width: float,
+        center: Tuple[float, float] = (0.0, 0.0),
+        mask_function: Callable[[torch.Tensor], torch.Tensor] | None = None,
+        mask_norm: float = 2 * torch.pi,
         mode: Literal[
             "nearest", "bilinear", "bicubic", "area", "nearest-exact"
         ] = "nearest",
@@ -35,83 +57,78 @@ class SpatialLightModulator(Element):
         super().__init__(simulation_parameters)
 
         self.mask = self.process_parameter("mask", mask)
-        self.height = self.process_parameter("height", height)
-        self.width = self.process_parameter("width", width)
-        self.step_function = step_function
-        _x, _y = location
-        self.x = self.process_parameter("x", _x)
-        self.y = self.process_parameter("y", _y)
+        self.height = height
+        self.width = width
+        self.center = center
+        self.mask_function = mask_function
+        self.mask_norm = mask_norm
+        self.mode = mode
 
-        self.mode = self.process_parameter("mode", mode)
-        self.number_of_levels = self.process_parameter(
-            "number_of_levels", number_of_levels
+        x = self.simulation_parameters.x
+        y = self.simulation_parameters.y
+
+        x_nodes = x.shape[0]
+        y_nodes = y.shape[0]
+        # Compute spatial grid spacing
+        dx = (x[1] - x[0]).cpu().item() if x_nodes > 1 else 1.0
+        dy = (y[1] - y[0]).cpu().item() if y_nodes > 1 else 1.0
+
+        x_intersection_slice, slm_x_intersection_slice = mesh_intersection_indices(
+            x[0].cpu().item(),
+            x[-1].cpu().item(),
+            self.center[0] - self.width / 2,
+            self.center[0] + self.width / 2,
+            dx,
         )
 
-        self.height_resolution, self.width_resolution = self.mask.shape
-
-        self._device = self.simulation_parameters.device
-
-        self._w_index = self.simulation_parameters.axes.index("x")
-        self._h_index = self.simulation_parameters.axes.index("y")
-
-        self._x_linear = self.make_buffer(
-            "_x_linear", self.simulation_parameters.axes.x
-        )
-        self._y_linear = self.make_buffer(
-            "_y_linear", self.simulation_parameters.axes.y
+        y_intersection_slice, slm_y_intersection_slice = mesh_intersection_indices(
+            y[0].cpu().item(),
+            y[-1].cpu().item(),
+            self.center[1] - self.height / 2,
+            self.center[1] + self.height / 2,
+            dy,
         )
 
-        self._x_grid = self._x_linear[None, :]
-        self._y_grid = self._y_linear[:, None]
+        # TODO: create slices for xy and mask meshgrids, continue rewritung from here
 
-    @property
-    def get_aperture(self) -> torch.Tensor:
+    def _resized_mask(self) -> torch.Tensor:
 
-        self.aperture = (
-            (torch.abs(self._x_grid - self.x) <= self.width / 2)
-            * (torch.abs(self._y_grid - self.y) <= self.height / 2)
-        ).to(dtype=torch.get_default_dtype())
-        return self.aperture
-
-    @property
-    def resized_mask(self) -> torch.Tensor:
-
-        _y_indices, _x_indices = torch.where(self.aperture == 1)
+        _y_indices, _x_indices = torch.where(self._aperture == 1)
         _y_indices, _x_indices = torch.unique(_y_indices), torch.unique(
             _x_indices
         )  # noqa: E501
 
-        self.left_boundary = _x_indices[
+        left_boundary = _x_indices[
             torch.argmin(
                 torch.abs(
-                    self._x_linear[_x_indices] - (self.x - self.width / 2)
+                    self._x_linear[_x_indices] - (self.center[0] - self.width / 2)
                 )  # noqa: E501
             ).item()
         ]
-        self.right_boundary = _x_indices[
+        right_boundary = _x_indices[
             torch.argmin(
                 torch.abs(
-                    self._x_linear[_x_indices] - (self.x + self.width / 2)
+                    self._x_linear[_x_indices] - (self.center[0] + self.width / 2)
                 )  # noqa: E501
             ).item()
         ]
-        self.top_boundary = _y_indices[
+        top_boundary = _y_indices[
             torch.argmin(
                 torch.abs(
-                    self._y_linear[_y_indices] - (self.y + self.height / 2)
+                    self._y_linear[_y_indices] - (self.center[1] + self.height / 2)
                 )  # noqa: E501
             ).item()
         ]
-        self.bottom_boundary = _y_indices[
+        bottom_boundary = _y_indices[
             torch.argmin(
                 torch.abs(
-                    self._y_linear[_y_indices] - (self.y - self.height / 2)
+                    self._y_linear[_y_indices] - (self.center[1] - self.height / 2)
                 )  # noqa: E501
             ).item()
         ]
 
-        x_nodes_interpolate = self.right_boundary - self.left_boundary + 1
-        y_nodes_interpolate = self.top_boundary - self.bottom_boundary + 1
+        x_nodes_interpolate = right_boundary - left_boundary + 1
+        y_nodes_interpolate = top_boundary - bottom_boundary + 1
 
         _resized_mask = self.mask.unsqueeze(0).unsqueeze(0)
 
@@ -135,8 +152,7 @@ class SpatialLightModulator(Element):
     @property
     def transmission_function(self) -> torch.Tensor:
 
-        _aperture = self.get_aperture
-        _resized_mask = self.resized_mask
+        _resized_mask = self._resized_mask()
 
         indices = (
             slice(self.bottom_boundary, self.top_boundary + 1),
