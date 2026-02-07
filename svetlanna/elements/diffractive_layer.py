419f8b9596f981f06edbd182d72c9122e6832161
djiboshin <64642744+djiboshin@users.noreply.github.com>
2026-02-07 23:54:40 +0300

    Update diffractive_layer.py
    * Fix docstrings

--- svetlanna/elements/diffractive_layer.py
+++ svetlanna/elements/diffractive_layer.py
@@ -9,28 +9,28 @@ from ..visualization import jinja_env, ElementHTML
 
 
 class DiffractiveLayer(Element):
-    """A class that described the field after propagating through the
-    passive diffractive layer with a given phase mask
-    """
-
     def __init__(
         self,
         simulation_parameters: SimulationParameters,
         mask: OptimizableTensor,
         mask_norm: float = 2 * torch.pi,
     ):
-        """Constructor method
+        r"""
+        Diffractive layer defined by a phase mask.
+        The field after propagating through the layer is calculated as:
+
+        $$
+        E^\text{out}_{xyw...} = E^\text{in}_{xyw...} \cdot \exp\left(2\pi i \frac{\text{mask}_{xy}}{\text{mask\_norm}}\right)
+        $$
 
         Parameters
         ----------
         simulation_parameters : SimulationParameters
-            Simulation parameters
+            Simulation parameters.
         mask : OptimizableTensor
-            Phase mask
+            Two-dimensional tensor representing the aperture mask of shape `(H, W)`.
         mask_norm : float, optional
-            This value will be used as following:
-            the phase addition is equal to `2*torch.pi * mask / mask_norm`.
-            By default, `2*torch.pi`
+            Mask normalization factor.
         """
 
         super().__init__(simulation_parameters)
@@ -40,41 +40,19 @@ class DiffractiveLayer(Element):
 
     @property
     def transmission_function(self) -> torch.Tensor:
+        r"""
+        The tensor representing the transmission function of the element
+        $\exp\left(2\pi i \dfrac{\text{mask}}{\text{mask\_norm}}\right)$.
+        The shape of the tensor is broadcastable to the incident wavefront's shape.
+        """
         return self.simulation_parameters.cast(
             torch.exp((2j * torch.pi / self.mask_norm) * self.mask), "y", "x"
         )
 
     def forward(self, incident_wavefront: Wavefront) -> Wavefront:
-        """Method that calculates the field after propagating through the SLM
-
-        Parameters
-        ----------
-        input_field : Wavefront
-            Field incident on the SLM
-
-        Returns
-        -------
-        Wavefront
-            The field after propagating through the SLM
-        """
         return incident_wavefront * self.transmission_function
 
     def reverse(self, transmission_wavefront: Wavefront) -> Wavefront:
-        """Method that calculates the field after passing the SLM in back
-        propagation
-
-        Parameters
-        ----------
-        transmitted_field : Wavefront
-            Field incident on the SLM in back propagation
-            (transmitted field in forward propagation)
-
-        Returns
-        -------
-        Wavefront
-            Field transmitted on the SLM in back propagation
-            (incident field in forward propagation)
-        """
         return transmission_wavefront * torch.conj(self.transmission_function)
 
     def to_specs(self) -> Iterable[ParameterSpecs]:
