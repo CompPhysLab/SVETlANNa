1baad2128ba68c70fd7422f6c300fbccf3e5b31f
djiboshin <64642744+djiboshin@users.noreply.github.com>
2026-03-24 02:17:46 +0300

    Create `test_spherical_wave.py` and `test_gaussian_beam.py`

--- tests/wavefront/test_wavefront.py
+++ tests/wavefront/test_wavefront.py
@@ -2,43 +2,6 @@ from svetlanna import Wavefront, SimulationParameters
 import torch
 import pytest
 
-import sympy as sp  # type: ignore
-
-
-def _gaussian_beam_symbolic():
-    x, y, w0, wavelength, z, dx, dy = sp.symbols("x y w0 wavelength z dx dy", real=True)
-
-    k = 2 * sp.pi / wavelength
-    zR = sp.pi * w0**2 / wavelength
-
-    w = w0 * sp.sqrt(1 + (z / zR) ** 2)
-    R = z * (1 + (zR / z) ** 2)
-    zeta = sp.atan(z / zR)
-
-    r2 = (x - dx) ** 2 + (y - dy) ** 2
-
-    E = (w0 / w) * sp.exp(-r2 / w**2) * sp.exp(sp.I * (k * z + k * r2 / (2 * R) - zeta))
-    E = sp.simplify(E)
-
-    return sp.lambdify((x, y, w0, wavelength, z, dx, dy), E, "numpy")
-
-
-def _spherical_wave_symbolic():
-    x, y, wavelength, z, dx, dy, phi0 = sp.symbols(
-        "x y wavelength z dx dy phi0", real=True
-    )
-
-    k = 2 * sp.pi / wavelength
-    r = sp.sqrt((x - dx) ** 2 + (y - dy) ** 2 + z**2)
-
-    E = (sp.S.One / r) * sp.exp(sp.I * (k * r + phi0))
-
-    return sp.lambdify((x, y, wavelength, z, dx, dy, phi0), E, "numpy")
-
-
-gaussian_beam_analytical = _gaussian_beam_symbolic()
-spherical_wave_analytical = _spherical_wave_symbolic()
-
 
 def test_creation():
     wf = Wavefront(1.0)
@@ -166,78 +129,6 @@ def test_plane_wave(distance, wavelength, initial_phase):
         Wavefront.plane_wave(
             sim_params, distance=distance, wave_direction=[dir_x, dir_y]
         )
-
-
-@pytest.mark.parametrize("distance", (1, 10, 100))
-@pytest.mark.parametrize("waist_radius", (1, 1.23, 1e-4, 1e4))
-@pytest.mark.parametrize("dx", (1.0, 123, 2e-4))
-@pytest.mark.parametrize("dy", (1.0, 123, 2e-4))
-@pytest.mark.parametrize("wavelength", (1.0, torch.tensor([1.23, 10.0, 40.0])))
-def test_gaussian_beam(distance, waist_radius, dx, dy, wavelength):
-    sim_params = SimulationParameters(
-        {
-            "x": torch.linspace(-0.1, 2, 10),
-            "y": torch.linspace(-1, 5, 20),
-            "wavelength": wavelength,
-        }
-    )
-
-    x = sim_params.cast(sim_params.x, "x")
-    y = sim_params.cast(sim_params.y, "y")
-    wl = sim_params.cast(sim_params.wavelength, "wavelength")
-
-    torch.testing.assert_close(
-        Wavefront.gaussian_beam(
-            sim_params, waist_radius=waist_radius, distance=distance, dx=dx, dy=dy
-        ),
-        torch.from_numpy(
-            gaussian_beam_analytical(
-                x=x.numpy(),
-                y=y.numpy(),
-                w0=waist_radius,
-                wavelength=wl.numpy(),
-                z=distance,
-                dx=dx,
-                dy=dy,
-            )
-        ),
-    )
-
-
-@pytest.mark.parametrize("distance", (1, 10, 100))
-@pytest.mark.parametrize("initial_phase", (1, 1.23, 1e-4, 20))
-@pytest.mark.parametrize("dx", (1.0, 123, 2e-4))
-@pytest.mark.parametrize("dy", (1.0, 123, 2e-4))
-@pytest.mark.parametrize("wavelength", (1.0, torch.tensor([1.23, 10.0, 40.0])))
-def test_spherical_wave(distance, initial_phase, dx, dy, wavelength):
-    sim_params = SimulationParameters(
-        {
-            "x": torch.linspace(-0.1, 2, 10),
-            "y": torch.linspace(-1, 5, 20),
-            "wavelength": wavelength,
-        }
-    )
-
-    x = sim_params.cast(sim_params.x, "x")
-    y = sim_params.cast(sim_params.y, "y")
-    wl = sim_params.cast(sim_params.wavelength, "wavelength")
-
-    torch.testing.assert_close(
-        Wavefront.spherical_wave(
-            sim_params, distance, initial_phase=initial_phase, dx=dx, dy=dy
-        ),
-        torch.from_numpy(
-            spherical_wave_analytical(
-                x=x.numpy(),
-                y=y.numpy(),
-                wavelength=wl.numpy(),
-                z=distance,
-                dx=dx,
-                dy=dy,
-                phi0=initial_phase,
-            )
-        ),
-    )
 
 
 def test_wavefront_as_a_tensor():
